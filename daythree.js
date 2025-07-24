3edb579e73e94581337e3c3980618bdc10ca9adc
alishbaamin518 <alishbaamin518@gmail.com>
2025-07-24 17:23:26 +0500

    concatination

---
+++ daythree.js
@@ -0,0 +1,11 @@
+// concatination
+var name = 'alishba'
+var Fname = 'Amin'
+var fullname = name + Fname
+console.log(fullname)
+// concatination hogaya dono mil ke likh ke a gaye 
+//  yeh sirf plus mai operate karega baki mai math hi
+console.log(3+3*"4"-2);  //=13 phele multiply then plus and minus
+console.log(2-"2"+6+8); // 14 first plus then minus and answer
+console.log(2+"2"+6-8); // first concatination 226 -8 = 218answer
+console.log(6+6+"32"+2-10); // first plus 12 concatination 12322 then minus =12312
