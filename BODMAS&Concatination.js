dfb4c9e4d02170873a77127975b5d31d42d35ad8
alishbaamin518 <alishbaamin518@gmail.com>
2025-08-16 03:27:55 +0500

    BODMAS&Concatination,prompt

---
+++ BODMAS&Concatination.js
@@ -0,0 +1,30 @@
+var cal = 5 + 5 * 3 - 2; 
+// bodmas rule apply kartain hain operations mai
+// bracket of 
+// divide
+// multiply 
+// addition 
+// substraction
+console.log(cal) //18
+//_________________CONCATINATION___________________
+ // js aagr sting ke sath plus karain gai tou concatination hoga
+ //  yeh sirf plus ke sign ke sath hoga 
+ // aagr sting pheli value hui  tou dusre ko bhi string consider kar ke concatination karega
+ //plus sign ke ilawa koi sign huwa tou operation perfome karega
+ //*string value black mai dekhe ga baatayega wo text hai
+ //* number in blue
+  var name = "alish"
+  var name2 = "ba"
+  var fullname = name+name2;
+  console.log(fullname);
+  console.log(3+3*"4"-2); //13
+  console.log(2-"2"+6+8); //14
+  console.log(2+"2"+6-8); //218
+  console.log(6+6+"32"+2-8); //12314
+  console.log(5/8+4-"8"+3*4-2); //6.625
+  //______________________Prompt
+  var name = +prompt("entre youre name")
+  console.log(name+20)
+  // prompt user ka data leta hai
+  // prompt se phele plus llagayain gai take concatination na hoo!
+  
